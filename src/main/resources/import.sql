ead1f8894e2a32e2bc6bf8eb0b5a586c2cede7e1
sistemaswebbrasil <adriano.faria@outlook.com.br>
2024-01-12 14:46:15 -0300

    Importação role inicial

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,2 +1,4 @@
+insert into role (id,name,description) values (1,'USER','Usuário comum');
+insert into role (id,name,description) values (2,'ADMIN','Usuário administrativo');
 insert into users (email,name,password,role,id) values ('user@user.com','Adriano Faria Alves','$2a$10$.nNh.Ve.ZdlsG45MwUhtMuUxo1cELrdP8LwvcqdAWOg6bH2ffmmNC','USER',9);
 insert into users (email,name,password,role,id) values ('admin@escritorio.com','administrador','$2a$10$.nNh.Ve.ZdlsG45MwUhtMuUxo1cELrdP8LwvcqdAWOg6bH2ffmmNC','ADMIN',10);
